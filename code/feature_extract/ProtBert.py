502183ecf378960bdbbd504f5b1fa84dbc254d65
harkic <99328605+harkic@users.noreply.github.com>
2024-02-04 23:04:20 +0800

    Update ProtBert.py

--- code/feature_extract/ProtBert.py
+++ code/feature_extract/ProtBert.py
@@ -49,21 +49,21 @@ all_protein_features = []
 for i, seq in enumerate(proBer_train_seq):
 
     ids = tokenizer1.batch_encode_plus([seq], add_special_tokens=True,
-                                      pad_to_max_length=True)  # encode_plus返回1、词的编码，2、
+                                      pad_to_max_length=True)  
     input_ids = torch.tensor(ids['input_ids']).to(device)
     attention_mask = torch.tensor(ids['attention_mask']).to(device)
     seqments_tensor = torch.tensor(ids["token_type_ids"]).to(device)
 
     with torch.no_grad():
         mbedding = model1(input_ids=input_ids, token_type_ids=seqments_tensor, attention_mask=attention_mask)[0]  # 二维的张量，获取模型最后一层的输出结果
-    embedding = embedding.cpu().numpy()  # 因为model是在gpu将其放在CPU上调 embeddnig转为数组
+    embedding = embedding.cpu().numpy()  
     features = []
     for seq_num in range(len(embedding)):
         seq_len = (attention_mask[seq_num] == 1).sum()
         seq_emd = embedding[seq_num][:seq_len - 1]
         features.append(seq_emd)
 
-    #         all_protein_features.append(seq_emd)
+
     output = model2(token_ids)
     unirep_output = output[0]
     unirep_output=torch.squeeze(unirep_output)
@@ -71,21 +71,20 @@ for i, seq in enumerate(proBer_train_seq):
     unirep_output = unirep_output.cpu().numpy()
     
     
-    all_protein_features += features  # 把一条一条的特征加到一块
+    all_protein_features += features 
 
-# 把所有提取到的特征放到DataFrame里
 sum_features = []
 for element in all_protein_features:
-    sum_features.append(element.sum(axis=0))  # np.mean(axis=0) protbert的特征可以用加的和平均的
+    sum_features.append(element.sum(axis=0)) 
 protBertfeature = pd.DataFrame(sum_features)
 
-#################################
+
 labels = trainlabel
-#######################
+
 all_labels = pd.DataFrame(labels)
 print(protBertfeature.shape)
 
 all_data = pd.concat((protBertfeature, all_labels), axis=1)
 
-with open("./XUprotbert_feature.csv", "w") as pf:
+with open("./protbert_feature.csv", "w") as pf:
     all_data.to_csv(pf, index=False, header=False)
